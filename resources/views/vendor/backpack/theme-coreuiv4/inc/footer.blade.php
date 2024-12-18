00f5713c83b6b21bd8bdb23c500ebac678d6e7eb
manish98625 <sraj09231@gmail.com>
2024-12-18 20:16:50 +0545

    alldata for export_data

---
+++ resources/views/vendor/backpack/theme-coreuiv4/inc/footer.blade.php
@@ -0,0 +1,5 @@
+@if (backpack_theme_config('show_powered_by') || backpack_theme_config('developer_link'))
+    <div class="m-auto ml-auto mr-auto text-muted p-2" style="width: fit-content">
+     
+    </div>
+@endif
